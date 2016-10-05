2ef47e934e1664019794c4a219bb4dc663f97ef6
sherri010 <aminshahidi.sh@gmail.com>
2016-10-05 15:52:19 -0700

    my solutions to sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,27 +11,38 @@ angular
   // the second argument is a function that defines the capacities
   // of the controller.
 
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+
+
+function AlbumsIndexController ( $http ) {
   var vm = this;
-  vm.newAlbum = {};
+  vm.newAlbum ={};
+  vm.albums;
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
+   $http({
+    method: 'GET',
+    url: '/api/albums',
+  }).then(function successCallback(response) {
+    console.log(response);
+    vm.albums = response.data ;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  vm.createAlbum = function () {
+    console.log(vm.newAlbum.name , vm.newAlbum.artistName , vm.newAlbum.genres , vm.newAlbum.releaseDate);
+  $http({
+    method: 'POST',
+    url: '/api/albums',
+    data: {name:vm.newAlbum.name,  artistName:vm.newAlbum.artistName , genres:vm.newAlbum.genres , releaseDate:vm.newAlbum.releaseDate},
+  }).then(function successCallback(response) {
+    console.log(response);
+    vm.albums.push(response.data)
+
+  }, function errorCallback(response) {
+    console.log('There was an error posting the data', response);
+  });
 }
+
+}
+
